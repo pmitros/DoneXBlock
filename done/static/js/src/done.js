090504c985d3da629283b7ebb9b20bf6dfb1f93b
Piotr Mitros <pmitros@mitx.mit.edu>
2014-11-05 12:01:13 -0500

    Accessibility+UX improvements

--- done/static/js/src/done.js
+++ done/static/js/src/done.js
@@ -1,9 +1,16 @@
+function update_knob(element, data) {
+  if($('.done_onoffswitch-checkbox', element).prop("checked")) {
+    $(".done_onoffswitch-switch", element).css("background-image", "url("+data['checked']+")");
+    $(".done_onoffswitch-switch", element).css("background-color", "#018801;");
+  } else {
+    $(".done_onoffswitch-switch", element).css("background-image", "url("+data['unchecked']+")");
+    $(".done_onoffswitch-switch", element).css("background-color", "#FFFFFF;");
+  }
+}
+
 function DoneXBlock(runtime, element, data) {
-    if (data.state) {
-	$('.done_windshield', element).addClass("done_windshield_off").removeClass("done_windshield_on");
-    } else {
-	$('.done_windshield', element).addClass("done_windshield_on").removeClass("done_windshield_off");
-    }
+    $('.done_onoffswitch-checkbox', element).prop("checked", data.state);
+    update_knob(element, data);
     var handlerUrl = runtime.handlerUrl(element, 'toggle_button');
 
     function updateCount(result) {}
@@ -12,16 +19,16 @@ function DoneXBlock(runtime, element, data) {
 	// Don't have animations on for above class changes. This is probably not necessary. I 
 	// was seeing animations on page load. I did a few things to fix it. The line below 
 	// wasn't the one that fixed it, but I decided to keep it anyways. 
-	$('.done_windshield', element).addClass("done_windshield_animated")
-	$('.done_windshield', element).click(function(){
-            $(this).toggleClass("done_windshield_on");
-            $(this).toggleClass("done_windshield_off");
+	//$('.done_block', element).addClass("done_windshield_animated");
+	$('.done_onoffswitch', element).addClass("done_animated");
+	$('.done_onoffswitch-checkbox', element).change(function(){
 	    $.ajax({
 		type: "POST",
 		url: handlerUrl,
-		data: JSON.stringify({"done":$(this).hasClass("done_windshield_off")}),
+		data: JSON.stringify({"done":$('.done_onoffswitch-checkbox', element).prop("checked")}),
 		success: updateCount
 	    });
+	    update_knob(element, data);
 	});
     });
 }
